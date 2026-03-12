7de4e9110a2ab2196ab973cbeccb450b9ff62d5f
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2026-03-12 11:46:57 +0200

    Add scattered brand icons (Teams, Meet, Notion, Zoom) around Kanban and Calendar sections

--- src/components/landing/LandingDemo.tsx
+++ src/components/landing/LandingDemo.tsx
@@ -3,12 +3,19 @@
 import { LandingDemoProvider } from "./LandingDemoContext";
 import { DemoKanban } from "./DemoKanban";
 import { CalendarDemo } from "./CalendarDemo";
+import { BrandBackground } from "./BrandBackground";
 
 export function LandingDemo() {
   return (
     <LandingDemoProvider>
-      <DemoKanban />
-      <CalendarDemo />
+      <div className="relative">
+        <BrandBackground />
+        <DemoKanban />
+      </div>
+      <div className="relative">
+        <BrandBackground />
+        <CalendarDemo />
+      </div>
     </LandingDemoProvider>
   );
 }
