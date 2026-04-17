307b86f1d4f35fc76ae6a830d1d421274f36c0f1
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2026-04-17 16:19:36 +0300

    chore: update src/components/landing/LandingDemo.tsx

--- src/components/landing/LandingDemo.tsx
+++ src/components/landing/LandingDemo.tsx
@@ -3,13 +3,11 @@
 import { LandingDemoProvider } from "./LandingDemoContext";
 import { DemoKanban } from "./DemoKanban";
 import { CalendarDemo } from "./CalendarDemo";
-import { BrandBackground } from "./BrandBackground";
 
 export function LandingDemo() {
   return (
     <LandingDemoProvider>
       <div className="relative">
-        <BrandBackground />
         <DemoKanban />
         <CalendarDemo />
       </div>
